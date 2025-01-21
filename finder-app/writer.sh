a2f8ebcec4808fdeca8b4b5b716cb85cb2ce914c
fed67 <you@example.com>
2025-01-21 22:14:08 +0100

    Created finder and writer bash files

---
+++ finder-app/writer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+writefile=$1
+writestr=$2
+
+if [ $# -ne 2 ]; then 
+    exit 1
+fi
+
+dir=$(dirname $writefile)
+
+if [ ! -e $dir  ]; then 
+    mkdir -p $dir
+fi
+
+if [ -e $writefile ]; then 
+    touch $writefile
+fi
+
+printf $writestr >> $writefile
+
+if [ $? -lt 0 ]; then
+    echo exited with error $?
+fi
